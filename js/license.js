32bc9e02aae8d67845537223f5e067f2811b242e
Anonymous <>
2022-04-27 21:09:05 -0700

    Update

--- js/license.js
+++ js/license.js
@@ -1,1 +1,1 @@
-!async function(){const t=await fetch("https://paste-tool.github.io/LICENSE.txt"),e=await t.text();document.getElementById("license-area").textContent=e}();
+!async function(){const t=await fetch(window.location.origin + "/LICENSE.txt"),e=await t.text();document.getElementById("license-area").textContent=e}();
